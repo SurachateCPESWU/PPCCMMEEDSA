1c18b2f6a2de77de0205435e7a3e54729062442e
SurachateCPESWU <surachate.frank@gmail.com>
2016-10-25 21:17:51 +0700

    เดหกด
    เดกหเหก

--- Arduino/test_class/test_class.ino
+++ Arduino/test_class/test_class.ino
@@ -1,36 +1,31 @@
-class Axis_X {
-    int Cx, Cy, sepX, dirX = 0 ;
+#include <TimedAction.h>
+
+
+class Axis {
+    int sep, dir = 0 ;
     boolean state_step = false;
     int step_count = 0;
-  public: Axis_X(int insepX, int indirX) {
-      sepX = insepX;
-      dirX = indirX;
-      pinMode(sepX, OUTPUT);
-      pinMode(dirX, OUTPUT);
-
+  public: Axis(int insep, int indir) {
+      sep = insep;
+      dir = indir;
+      pinMode(sep, OUTPUT);
+      pinMode(dir, OUTPUT);
 
     }
 
-  public: void setOrigin() {
-      Cx = 0 ;
-      Cy = 0 ;
-    }
 
   public: void setStep(int setStep) {
       step_count = setStep;
     }
 
-  public: String where() {
-      return Cx + "," + Cy;
-    }
 
   public: void moveTo() {
       if (state_step == false) {
-        digitalWrite(sepX, HIGH);
+        digitalWrite(sep, HIGH);
         state_step = true;
         step_count--;
       } else {
-        digitalWrite(sepX, LOW);
+        digitalWrite(sep, LOW);
         state_step = false;
         step_count--;
       }
@@ -38,67 +33,32 @@ class Axis_X {
 
   public: void setDir(boolean dir) {
       if (dir == true) {
-        digitalWrite(dirX, LOW);
+        digitalWrite(dir, LOW);
       } else {
-        digitalWrite(dirX, HIGH);
+        digitalWrite(dir, HIGH);
       }
       delay(100);
     }
 
+
+
 };
 
 
-class Axis_Y {
-    int Cx, Cy, sepX, dirX = 0 ;
-    boolean state_step = false;
-    int step_count = 0;
-  public: Axis_Y(int insepX, int indirX) {
-      sepX = insepX;
-      dirX = indirX;
-      pinMode(sepX, OUTPUT);
-      pinMode(dirX, OUTPUT);
 
+Axis x(10, 11);
+Axis y(12, 13);
 
-    }
+void gogox() {
+  x.moveTo();
+}
 
-  public: void setOrigin() {
-      Cx = 0 ;
-      Cy = 0 ;
-    }
+void gogoy() {
+  y.moveTo();
+}
 
-  public: void setStep(int setStep) {
-      step_count = setStep;
-    }
-
-  public: String where() {
-      return Cx + "," + Cy;
-    }
-
-  public: void moveTo() {
-      if (state_step == false) {
-        digitalWrite(sepX, HIGH);
-        state_step = true;
-        step_count--;
-      } else {
-        digitalWrite(sepX, LOW);
-        state_step = false;
-        step_count--;
-      }
-    }
-
-  public: void setDir(boolean dir) {
-      if (dir == true) {
-        digitalWrite(dirX, LOW);
-      } else {
-        digitalWrite(dirX, HIGH);
-      }
-      delay(100);
-    }
-
-};
-
-Axis_X x(10, 11);
-Axis_Y y(12, 13);
+TimedAction timedAction = TimedAction(5, gogox);
+TimedAction timedAction2 = TimedAction(17, gogoy);
 void setup() {
 
   Serial.begin(9600);
@@ -106,19 +66,22 @@ void setup() {
 }
 
 void loop() {
-  x.setDir(true);
-  y.setDir(true);
-  for (int i = 0 ; i <= 600 ; i++) {
-    x.moveTo();
-    y.moveTo();
-    delay(15);
-  }
-  x.setDir(false);
-  y.setDir(false);
-  for (int i = 0 ; i <= 600 ; i++) {
-    x.moveTo();
-    y.moveTo();
-    delay(15);
-  }
+
+  timedAction.check();
+  timedAction2.check();
+  //  x.setDir(true);
+  //  y.setDir(true);
+  //  for (int i = 0 ; i <= 600 ; i++) {
+  //    x.moveTo();
+  //    y.moveTo();
+  //    delay(15);
+  //  }
+  //  x.setDir(false);
+  //  y.setDir(false);
+  //  for (int i = 0 ; i <= 600 ; i++) {
+  //    x.moveTo();
+  //    y.moveTo();
+  //    delay(15);
+  //  }
 
 }
